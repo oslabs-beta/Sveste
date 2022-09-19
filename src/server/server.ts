e0725a724672a46985c08a8f67be46aac722314d
Jeremy Vogel <jeremyvogel@Jeremys-MBP.fios-router.home>
2022-09-19 16:25:10 -0400

    server boilerplate

---
+++ src/server/server.ts
@@ -0,0 +1,17 @@
+// server boilerplate here.
+
+import * as express from 'express';
+//above may require the below in tsconfig
+//"allowSyntheticDefaultImports": true 
+import type { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
