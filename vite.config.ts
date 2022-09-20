4b0d7a09e6768a097acddd2d4bcc4da823b4a251
sagarvxyz <sagar.velagala@gmail.com>
2022-09-20 10:43:37 -0400

    sync w/ dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/server/server.ts",
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
