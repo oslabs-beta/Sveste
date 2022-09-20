514e845670d9ee60adf50097a14bc745b9cf48e9
Marco Kwan <mrcokwn@gmail.com>
2022-09-20 10:23:05 -0400

    codeeditorpart

---
+++ src/components/CopyToClipboard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  export let text: string;
+
+  const dispatch = createEventDispatcher<{ copy: string; fail: never }>();
+
+  const copy = () => {
+    navigator.clipboard.writeText(text).then(
+      () => dispatch("copy", text),
+      (e) => dispatch("fail")
+    );
+  };
+</script>
+
+<slot {copy} />
