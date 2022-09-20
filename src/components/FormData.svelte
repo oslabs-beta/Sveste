9c5f36ee878571f1bb2a56279b79c0f5ab41dc1d
sagarvxyz <sagar.velagala@gmail.com>
2022-09-19 22:10:52 -0400

    refactor: htmlTag as a text input

--- src/components/FormData.svelte
+++ src/components/FormData.svelte
@@ -1,36 +1,39 @@
-<script>
+<script lang="ts">
   import { POST } from "../controllers/submitForm";
-  let selected;
-  let options = ["Render", "Event"];
-  function handleSubmit(event) {
-    const formData = new FormData(event.target);
+  let htmlTag = "body";
+
+  function handleSubmit() {
+    const form = document.querySelector("form");
+    const formData = new FormData(form);
     const testParam = new URLSearchParams();
-    testParam.append("testType", selected);
     const testBody = {};
     for (const [key, value] of formData) {
-      testBody[key] = value;
+      if (key === "htmlTag" && typeof value === "string") {
+        testParam.append(key, value);
+      } else {
+        testBody[key] = value;
+      }
     }
-
     let response = POST("/test?" + testParam, JSON.stringify(testBody));
   }
 </script>
 
-<form
-  class="form-container"
-  on:submit|preventDefault={(event) => handleSubmit(event)}
->
-  <label for="componentName">Component Name:</label>
+<form class="form-container" on:submit|preventDefault={handleSubmit}>
+  <label for="htmlTag">HTML Tag</label>
+  <input required type="text" name="htmlTag" placeholder="h1" />
+
+  <label for="componentName">Component Name</label>
   <input required type="text" name="componentName" placeholder="Header" />
 
-  <label for="relativePath">Relative Path:</label>
+  <label for="relativePath">Relative Path</label>
   <input required type="text" name="relativePath" placeholder="/tests" />
 
-  <label for="testName">Test Name:</label>
+  <label for="testName">Test Name</label>
   <input required type="text" name="testName" placeholder="Header rendered" />
 
-  <label for="expectedResult">Expected Result:</label>
+  <label for="expectedResult">Expected Result</label>
   <input required type="text" name="expectedResult" placeholder="Hello World" />
-  <input type="submit" value="Submit" />
+  <button type="submit">Submit</button>
 </form>
 
 <style>
