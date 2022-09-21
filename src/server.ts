88125281958ec2984b4e51ac9c9be19d125b0724
dtam1997 <derektam1997@gmail.com>
2022-09-21 07:38:56 -0700

    Update 2 Sep 21, 2022

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,16 @@ import type { Express, Request, Response } from "express";
 const app: Express = express();
 const port = 3000;
 
+/*
+ * require routers
+ */
+import components from "./routes/routes";
+
 app.get("/", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
+
+app.use("/tests", components);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
