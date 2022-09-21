d5738f2855c57a61729b9aa94f45d67bdc2794d2
sagarvxyz <sagar.velagala@gmail.com>
2022-09-21 16:48:02 -0400

    feat: use map instead of object for store

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,34 @@
 import { writable } from "svelte/store";
 
-function createStore() {
-  const { subscribe, set, update } = writable({});
+// function createStore() {
+//   const { subscribe, set, update } = writable({});
 
-  return {
-    subscribe,
-    addGroup: (key, obj) => {
-      update(() => (testStore[key] = obj));
-      console.log(testStore);
-    },
-    deleteGroup: (key) => {
-      update(() => delete testStore[key]);
-      console.log(testStore);
-    },
+//   return {
+//     subscribe,
+//     addGroup: (key, obj) => {
+//       update(() => testStore[key] = obj);
+//       console.log(testStore);
+//     },
+//     deleteGroup: (key) => {
+//       update(() => delete testStore[key]);
+//       console.log(testStore);
+//     }
+//   };
+// }
+
+function createStore() {
+  const map = new Map();
+  const { subscribe, set, update } = writable({});
+  map["subcribe"] = subscribe;
+  map["addGroup"] = (key, obj) => {
+    update(() => testStore.set(key, obj));
+    console.log(testStore);
   };
+  map["deleteGroup"] = (key) => {
+    update(() => testStore.delete(key));
+    console.log(testStore);
+  };
+  return map;
 }
 
 export const testStore = createStore();
