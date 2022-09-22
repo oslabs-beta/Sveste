47c565773f1cd12e8d129ef21055cfec661cde1b
sagarvxyz <sagar.velagala@gmail.com>
2022-09-22 14:20:56 -0400

    feat: AddButton

---
+++ src/components/form/AddButtonOptions.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { testStore } from "../../models/store";
+  import { Block } from "../../controllers/blockClass";
+
+  function addGroup() {
+    const newGroup = new Block("group");
+    testStore.addBlock(newGroup);
+    // groups = [...groups, newGroup];
+  }
+</script>
+
+<menu>
+  <h3>Add new...</h3>
+  <button on:click|preventDefault={addGroup}>Group</button>
+</menu>
+
+<style>
+  menu {
+    border: 1px solid var(--primary);
+    padding: 1rem;
+    list-style-type: none;
+  }
+</style>
