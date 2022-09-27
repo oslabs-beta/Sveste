c8562f6dbb8d5dca6eec5fcf52e2cdc4c022aac2
sagarvxyz <sagar.velagala@gmail.com>
2022-09-27 15:05:43 -0400

    feat: mock label

--- src/components/form/Mock.svelte
+++ src/components/form/Mock.svelte
@@ -5,12 +5,17 @@
 </script>
 
 <div>
-  <label for="mockInput">Mock</label>
-  <select id="mockInput" name="mockInput" bind:value={$blockStore[id]["value"]}>
+  <select
+    required
+    id="mockInput"
+    name="mockInput"
+    bind:value={$blockStore[id]["value"]}
+  >
     {#each options as option}
       <option value={option}>{option}</option>
     {/each}
   </select>
+  <label for="mockInput">Mock</label>
 </div>
 
 <style>
@@ -21,10 +26,26 @@
   }
   select {
     width: 100%;
+    height: 2.5rem;
+    font-size: 0.9rem;
+    padding-top: 0.5rem;
+    padding-left: 0.1rem;
+  }
+  option {
+    bottom: 0;
+    /* padding: 0.6rem 0 0 0.4rem; */
   }
   label {
     position: absolute;
-    left: 0.25rem;
-    z-index: 1;
+    left: 0.5rem;
+    top: 0.75rem;
+    transform-origin: left top;
+    transition: transform 100ms ease-in-out;
+  }
+
+  select:focus + label,
+  select:valid + label {
+    transform: translateY(-50%) scale(0.7);
+    color: var(--secondary);
   }
 </style>
