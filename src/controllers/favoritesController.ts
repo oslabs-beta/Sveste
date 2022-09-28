26d5eddba7e5907534d3344647de779d8480c1ee
Adam Seery <adam@Adams-MacBook-Air.local>
2022-09-28 10:01:57 -0600

    working add favorite controller and route

--- src/controllers/favoritesController.ts
+++ src/controllers/favoritesController.ts
@@ -1,3 +1,37 @@
-const favoritesController = {};
+import type { Request, Response, NextFunction, RequestHandler } from "express";
+import type User from "../models/models";
+import { collections } from "../services/database.services";
+type FavoritesController = {
+  addFavorite: RequestHandler;
+};
 
-export default favoritesController;
+export const favoritesController: FavoritesController = {
+  addFavorite: async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const { _id, favorite } = req.body;
+      //   const currentUser = await collections.SvesteUsers.findOne({
+      //     _id: _id,
+      //   });
+      const currentUser = await collections.SvesteUsers.updateOne(
+        { _id: _id },
+        {
+          $push: { storage: favorite },
+        }
+      );
+      //     console.log(currentUser);
+      //   console.log(req.body);
+      //   currentUser.storage.push(favorite);
+      //  const update = mongooseUser.save()
+      //   console.log(updatedUser);
+      //   res.locals.favorites = updatedUser;
+      res.locals.favorites = currentUser;
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+  },
+};
--- src/routers/favoritesRouter.ts
+++ src/routers/favoritesRouter.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import type { Request, Response } from "express";
+import { favoritesController } from "../controllers/favoritesController";
 const favoritesRouter = express.Router();
 favoritesRouter.use(express.json());
 
-favoritesRouter.get("/", (req: Request, res: Response) => {
-  return res.status(200).send("get request working");
-});
+favoritesRouter.get(
+  "/",
+  favoritesController.addFavorite,
+  (req: Request, res: Response) => {
+    return res.status(200).send(res.locals.favorites);
+  }
+);
 favoritesRouter.post("/", (req: Request, res: Response) => {
   return res.status(200).send("post request working");
 });
