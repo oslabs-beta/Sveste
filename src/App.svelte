4bb46703fa121267ad9c4b69f7fc0b30020db8d0
Marco Kwan <mrcokwn@gmail.com>
2022-09-28 17:11:29 -0400

    app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,8 @@
   import NotFound from "./routes/NotFound.svelte";
   import Home from "./pages/Home.svelte";
   import Favorites from "./pages/Favorites.svelte";
+  import { isLoggedIn } from "./models/store";
+  import { push } from "svelte-spa-router";
 
   export const loadCookie = () => {
     const myCookieValue = document.cookie;
@@ -18,7 +20,24 @@
     "/signup": SignUp,
     "/login": Login,
     "/home": Home,
-    "/favorites": Favorites,
+    "/favorites": wrap({
+      // The Svelte component used by the route
+      component: Favorites,
+      // List of route pre-conditions
+      conditions: [
+        // First pre-condition function
+        (detail) => {
+          /********PLACEHOLDER FOR ROUTING CONDITION**********/
+          if (($isLoggedIn = false)) {
+            push("/signup");
+            return false;
+          } else {
+            push("/favorites");
+            return true;
+          }
+        },
+      ],
+    }),
     "*": NotFound,
   };
 </script>
