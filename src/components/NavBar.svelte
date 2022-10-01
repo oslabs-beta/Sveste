8c1ee01231abd5805a4a1c1cf67aafef6eddac55
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-10-01 14:20:43 -0600

    recursive rendering with block type working

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
   import NavBlock from "./NavBlock.svelte";
+  import { blockStore, testStore } from "../models/store";
+  import { children } from "svelte/internal";
   // dummy data for now
+  $: $testStore && populateParents();
+  const parentArray = [];
+  function populateParents() {
+    const describeArr = $testStore["children"];
+    // console.log(describeArr);
+    if (describeArr) {
+      describeArr.forEach((describeObj) => {
+        const obj = {
+          blockType: "parent",
+          children: [],
+          type: describeObj.type,
+        };
+        // obj.children.push(processChildren(describeObj));
+        parentArray.push(obj);
+        console.log(parentArray);
+      });
+    }
+  }
+  function processChildren(blockObj, result = []) {
+    if (!blockObj) return result;
+    const childObj = {};
+  }
+  function handleClick() {
+    // console.log('blockstore', $blockStore);
+    console.log("teststore", $testStore["children"]);
+  }
 </script>
 
 <div>
-  <NavBlock blockType="parent" />
+  <NavBlock
+    blockType={"parent"}
+    name={$testStore["type"]}
+    children={$testStore["children"]}
+  />
+  <button on:click|preventDefault={() => populateParents()}>Test</button>
 </div>
--- src/components/NavBlock.svelte
+++ src/components/NavBlock.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
   import ExpandMore from "./icons/ExpandMore.svelte";
   import ExpandLess from "./icons/ExpandLess.svelte";
+  import AddButton from "./form/AddButton.svelte";
+  // import { children } from 'svelte/internal';
 
   let isOpen = false;
   const toggleMenu = () => {
     console.log("clicked");
+    console.log(blockType);
     isOpen = !isOpen;
   };
 
   // dummy data, use svelte:self with a tree of components later
-  export let blockType: "parent" | "child";
-  let children = [
-    { blockType: "child" },
-    { blockType: "child" },
-    { blockType: "child" },
-  ];
+  // export let blockType: "parent" | "child";
+  export let blockType;
+  export let name;
+  export let children = [];
+  // let children = [
+  //   { blockType: "child" },
+  //   { blockType: "child" },
+  //   { blockType: "child" },
+  // ];
   let indent = 1.5;
 </script>
 
@@ -29,13 +35,20 @@
     <p>&ensp;</p>
   {/if}
 
-  <p>{blockType}</p>
+  <!-- <p>{blockType}</p> -->
+  {name}
 </button>
 
 <div style="padding-left: {indent}rem;">
   {#if isOpen}
     {#each children as child}
-      <button><p>{child.blockType}</p></button>
+      {#if child.children}
+        {#if child.type === "describe" || child.type === "root" || child.type === "test" || child.type === "mock"}
+          <svelte:self {...child} name={child.type} blockType={"parent"} />
+        {:else}
+          <svelte:self {...child} name={child.type} blockType={"child"} />
+        {/if}
+      {/if}
     {/each}
   {/if}
 </div>
