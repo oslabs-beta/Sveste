31ccf271f17d3b05fed4f67b5351012b555c15f8
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-10-03 17:13:03 -0600

    cleaned up extra component files and updated test compile with new types

--- src/controllers/blockTypes.ts
+++ src/controllers/blockTypes.ts
@@ -1,26 +1,16 @@
-import DescribeStatement from '../components/statements/describeStatement.svelte';
-import Mock from '../components/form/Mock.svelte';
-import MockStatement from '../components/form/MockStatement.svelte';
-import RenderBlock from '../components/blocks/renderBlock.svelte';
-import ModierBlock from '../components/blocks/modifierBlock.svelte';
-import Query from '../components/form/Query.svelte';
-import Assertion from '../components/form/Assertion.svelte';
-import UserEvent from '../components/form/UserEvent.svelte';
-import ExecutionBlock from '../components/blocks/executionBlock.svelte';
-import TestStatement from '../components/statements/testStatement.svelte';
-import ModifierBlock from '../components/blocks/modifierBlock.svelte';
+import DescribeStatement from "../components/statements/describeStatement.svelte";
+import RenderBlock from "../components/blocks/renderBlock.svelte";
+import ExecutionBlock from "../components/blocks/executionBlock.svelte";
+import TestStatement from "../components/statements/testStatement.svelte";
+import ModifierBlock from "../components/blocks/modifierBlock.svelte";
 
 type TypeComponentPair = { type: string; component: any };
 type BlockRef = TypeComponentPair[];
 
 export const blockRef: BlockRef = [
-  { type: 'describeStatement', component: DescribeStatement },
-  { type: 'mock', component: Mock },
-  { type: 'modifierBlock', component: ModifierBlock },
-  { type: 'renderBlock', component: RenderBlock },
-  { type: 'query', component: Query },
-  { type: 'testStatement', component: TestStatement },
-  { type: 'executionBlock', component: ExecutionBlock },
-  { type: 'assertion', component: Assertion },
-  { type: 'event', component: UserEvent },
+  { type: "describeStatement", component: DescribeStatement },
+  { type: "modifierBlock", component: ModifierBlock },
+  { type: "renderBlock", component: RenderBlock },
+  { type: "testStatement", component: TestStatement },
+  { type: "executionBlock", component: ExecutionBlock },
 ];
