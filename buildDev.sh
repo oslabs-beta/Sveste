4ad092f41d68b5b01b9263c385335e07642faa5d
sagarvxyz <sagar.velagala@gmail.com>
2022-10-11 14:52:42 -0400

    build: aws config

---
+++ buildDev.sh
@@ -0,0 +1,17 @@
+docker run -d -p 8000:8000 amazon/dynamodb-local
+
+AWS_PAGER="" aws dynamodb create-table \
+--table-name FavoritesTable \
+--attribute-definitions AttributeName=ID,AttributeType=S \
+--key-schema AttributeName=ID,KeyType=HASH \
+--endpoint-url http://localhost:8000 \
+--billing-mode PAY_PER_REQUEST
+
+AWS_PAGER="" aws dynamodb create-table \
+--table-name UsersTable \
+--attribute-definitions AttributeName=ID,AttributeType=S \
+--key-schema AttributeName=ID,KeyType=HASH \
+--endpoint-url http://localhost:8000 \
+--billing-mode PAY_PER_REQUEST
+
+sam local start-api & vite
